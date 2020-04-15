863c7f5b1b4817ff57f9b935d3143fb91c040ad7
nuribarri <36167128+nuribarri@users.noreply.github.com>
2020-04-14 23:44:30 -0600

    Update deck.h

--- deck.h
+++ deck.h
@@ -29,30 +29,75 @@ class Deck
     Deck();           // pristine, sorted deck
 
     void shuffle();   // shuffle the deck, all 52 cards present
-    Card dealCard();   // get a card, after 52 are dealt, fail 
+    Card dealCard();   // get a card, after 52 are dealt, fail to deal if empty
 
     int  size() const; // # cards left in the deck
 
   private:
 
-    Card myCards[SIZE];
+    Card myCards[SIZE];             // The deck of cards as an array
     int myIndex;  // current card to deal
 };
 
 Deck::Deck() {
 
+    myIndex = 0;
+    int i = 0;      //Creating a tempory index
+    int suit;
+    int rank;
+
+    for(suit = 0; suit < 4; suit++){
+        for(rank = 1; rank < 14; rank++){
+
+            myCards[i] = Card(rank, Card::Suit(suit));      // adding one card at a time by suit and rank
+            i++;
+        }
+    }
+
 }
 
 void Deck::shuffle() {
 
+    Card swap;
+    int i = 0;
+    int rand1;
+    int rand2;
+
+    for(i = 0; i < SIZE * 2; i++){
+
+        rand1 = rand() % SIZE;
+        rand2 = rand() % SIZE;
+
+        swap = myCards[rand1];              // store first random card temporarily
+        myCards[rand1] = myCards[rand2];    // swap card 1 with card 2
+        myCards[rand2] = swap;              // store card 2 with card 1
+    }
+
 }
 
 Card Deck::dealCard() {
-    return Card();
+
+    Card dealt(0,Card::Suit(Card::spades)); // returns Ace of Spades if something goes wrong
+
+    if(size() > 0) {
+
+        dealt = myCards[myIndex];
+        myIndex = myIndex + 1;
+        return dealt;
+
+    }
+    return dealt;
 }
 
+
 int Deck::size() const {
-    return 0;
+
+    int sizeofDeck;
+
+    sizeofDeck = SIZE - myIndex;
+
+    return sizeofDeck;
+
 }
 
 #endif
